b15f6fb4fd8dce76fe2d5b6f68ba08b6a2d95de7
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-06-07 09:10:13 -0300

    fix: delete CrmContactCrudController stub (replaced by ContactCrudController)

--- src/Controller/Admin/CrmContactCrudController.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Este arquivo foi intencionalmente esvaziado.
-// O controller foi renomeado para ContactCrudController.php
-// para evitar colisão de rotas com CrmController.
-// Pode ser deletado com segurança.
