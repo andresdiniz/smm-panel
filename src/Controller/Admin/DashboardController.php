cd1f6a0b254f77d7228c5c24594ae263aff269bc
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-07-29 18:41:31 -0300

    fix: DashboardController extends AbstractDashboardController to inject EasyAdmin context (fixes ea.i18n null error)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,14 +9,16 @@ use App\Repository\ProviderCredentialRepository;
 use App\Repository\ServiceRepository;
 use App\Repository\UserRepository;
 use App\Repository\WalletTransactionRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin', name: 'admin_')]
 #[IsGranted('ROLE_ADMIN')]
-class DashboardController extends AbstractController
+class DashboardController extends AbstractDashboardController
 {
     public function __construct(
         private readonly OrderRepository              $orderRepo,
@@ -45,5 +47,19 @@ class DashboardController extends AbstractController
             'recentOrders' => $this->orderRepo->findRecent(10),
             'recentUsers'  => $this->userRepo->findRecent(10),
         ]);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('SMM Panel Admin');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Usuários', 'fa fa-users', \App\Entity\User::class);
+        yield MenuItem::linkToCrud('Pedidos', 'fa fa-shopping-cart', \App\Entity\Order::class);
+        yield MenuItem::linkToCrud('Serviços', 'fa fa-cogs', \App\Entity\Service::class);
     }
 }
